1ad937be0f36f33681f8ec644021afaef1ac75ae
Riari <3583774+Riari@users.noreply.github.com>
2024-09-15 19:51:20 +0100

    Improve cat movement

--- scripts/characters/cat.gd
+++ scripts/characters/cat.gd
@@ -15,22 +15,23 @@ var state_to_anim = {
 	State.FALL: "Fall",
 }
 
-var state_to_speed = {
-	State.IDLE: 0.0,
-	State.WALK: 40.0,
-	State.RUN: 150.0,
-	State.FALL: 40.0
+const GRAVITY = 150.0
+
+var state_to_velocity = {
+	State.IDLE: Vector2(0.0, 0.0),
+	State.WALK: Vector2(40.0, 0.0),
+	State.RUN: Vector2(150.0, 0.0),
+	State.FALL: Vector2(40.0, GRAVITY),
 }
 
 var current_state: State = State.IDLE
 var state_change_interval = [2.0, 5.0]
 var next_state_change_in: float
 var state_change_timer = 0.0
+var direction = 1.0
 var was_on_floor = true
 
 var rng = RandomNumberGenerator.new()
-
-const GRAVITY = 150.0
 
 @onready var sprite = get_node("AnimatedSprite2D")
 
@@ -39,36 +40,32 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if !was_on_floor && is_on_floor():
-		execute_next_state()
-		velocity = Vector2(state_to_speed[current_state], 0.0)
+		execute_random_state()
 		was_on_floor = true
 	elif was_on_floor && !is_on_floor():
 		execute_state(State.FALL)
 		was_on_floor = false
-		velocity = Vector2(state_to_speed[current_state], GRAVITY)
-		print("Started falling")
-
-	if is_on_floor():
+	elif was_on_floor && is_on_floor():
 		state_change_timer += delta
 		if state_change_timer >= next_state_change_in:
-			execute_next_state()
-			sprite.play(state_to_anim[current_state])
+			execute_random_state()
 			state_change_timer = 0
 
-			velocity = Vector2(state_to_speed[current_state], 0.0)
-			var should_change_direction = rng.randi_range(0, 1)
-			if should_change_direction == 1:
-				velocity.x *= -1.0
-
-			if current_state != State.IDLE:
-				sprite.flip_h = velocity.x < 0.0
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func execute_state(state: State) -> void:
 	current_state = state
+
+	if current_state not in [State.IDLE, State.FALL]:
+		var should_change_direction = rng.randi_range(0, 1)
+		if should_change_direction == 1:
+			scale.x = -scale.x
+			direction = -direction
+
 	sprite.play(state_to_anim[current_state])
+	var new_velocity = state_to_velocity[current_state]
+	velocity = Vector2(new_velocity.x * direction, new_velocity.y)
 
 func pick_next_state() -> State:
 	var new_state = State.values().pick_random()
@@ -77,7 +74,7 @@ func pick_next_state() -> State:
 	else:
 		return new_state
 
-func execute_next_state() -> void:
+func execute_random_state() -> void:
 	execute_state(pick_next_state())
 	pick_next_state_change_interval()
 
