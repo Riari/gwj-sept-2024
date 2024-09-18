99ec71ece86ac3fdf7712388350f3fd1876beba1
Riari <3583774+Riari@users.noreply.github.com>
2024-09-18 11:10:28 +0100

    Improve shop implementation

--- scripts/ui/shop.gd
+++ scripts/ui/shop.gd
@@ -1,20 +1,43 @@
 extends Control
 
-const FILE_JSON_TOWERS = "res://data/towers.json"
+signal item_purchased(item_data: Dictionary)
 
-@export var item_preview_scene = preload("res://scenes/ui/item-preview.tscn")
+const FILE_JSON_ITEMS = "res://data/items.json"
 
-@onready var towers_section: GridContainer = $Towers
+@export var item_scene = preload("res://scenes/ui/shop-item.tscn")
+
+@onready var inventory: GridContainer = $InventoryContainer/MarginContainer/Inventory
+@onready var selected_item_description: Label = $SelectedItemDescription
+@onready var button_buy: Button = $ButtonBuy
+
+var selected_item_data = {}
 
 func _ready() -> void:
 	var utils = Utils.new()
-	var tower_data = utils.load_json(FILE_JSON_TOWERS)
+	var item_data = utils.load_json(FILE_JSON_ITEMS)
 	
-	for tower in tower_data["Towers"]:
-		var preview_scene = item_preview_scene.instantiate()
-		print(tower)
-		var preview_image = Image.load_from_file(tower["PreviewImage"])
-		preview_scene.item_image_texture = ImageTexture.create_from_image(preview_image)
-		preview_scene.item_name = tower["Name"]
-		preview_scene.item_price = int(tower["Price"])
-		towers_section.add_child(preview_scene)
+	for item in item_data["Items"]:
+		var item_node = item_scene.instantiate()
+		var preview_image = Image.load_from_file(item["PreviewImage"])
+		item_node.item_image_texture = ImageTexture.create_from_image(preview_image)
+		item_node.item_data = item
+		inventory.add_child(item_node)
+		item_node.connect("item_selected", _on_item_selected)
+		item_node.connect("item_deselected", _on_item_deselected)
+
+func _on_button_close_pressed() -> void:
+	visible = false
+
+func _on_button_buy_pressed() -> void:
+	visible = false
+	item_purchased.emit(selected_item_data)
+
+func _on_item_selected(item_data: Dictionary) -> void:
+	selected_item_data = item_data
+	selected_item_description.text = item_data["Description"]
+	button_buy.disabled = false
+
+func _on_item_deselected() -> void:
+	selected_item_data = {}
+	selected_item_description.text = ""
+	button_buy.disabled = true
---
+++ scripts/ui/shop_item.gd
@@ -0,0 +1,22 @@
+extends BaseButton
+
+signal item_selected(item_data: Dictionary)
+signal item_deselected
+
+var item_image_texture: ImageTexture
+var item_data = {}
+
+@onready var item_image: TextureRect = $ImageContainer/Image
+@onready var item_name_label: Label = $NameLabel
+@onready var item_price_label: Label = $PriceLabel
+
+func _ready() -> void:
+	item_image.texture = item_image_texture
+	item_name_label.text = item_data["Name"]
+	item_price_label.text = "%d" % item_data["Price"]
+
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		item_selected.emit(item_data)
+	else:
+		item_deselected.emit()
